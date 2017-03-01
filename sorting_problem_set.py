7ebaee08790f7c8077f7c312e91a2c2d4a2986b8
nsachs <nsachs@fwparker.org>
2017-03-01 17:14:27 -0600

    done

--- sorting_problem_set.py
+++ sorting_problem_set.py
@@ -58,4 +58,36 @@ print(sorting(sort_me2))
 
 sort_me3 = [77, 29, 59, 69, 86, 40, 47, 40, 74, 44, 58, 78, 9, 8, 13, 99, 3, 57, 19, 48, 55, 50, 94, 69, 98, 30, 37, 29, 40, 29, 36, 32, 26, 85, 61, 51, 70, 96, 90, 89, 91, 88, 68, 4, 4, 74, 15, 72, 5, 91, 76, 6, 56, 80, 72, 87, 63, 86, 48, 24, 17, 23, 30, 41, 7, 64, 16, 19, 40, 63, 14, 95, 44, 58, 1, 6, 45, 55, 52, 54, 44, 36, 50, 6, 96, 66, 8, 46, 48, 48, 75, 25, 39, 30, 70, 44, 38, 90, 49, 70]
 
-def sorting1(sort_me3):
+
+number_of_times = 0
+for pos in range(len(sort_me3)):
+    min_pos = pos
+    number_of_times += 1
+    for scan_pos in range(min_pos, len(sort_me3)):
+        if sort_me3[scan_pos] < sort_me3[min_pos]:
+            min_pos = scan_pos
+    temp = sort_me3[pos]
+    sort_me3[pos] = sort_me3[min_pos]
+    sort_me3[min_pos] = temp
+
+
+print("With selection sorting: iterated through the list", number_of_times, "times.")
+print()
+
+number_of_times = 0
+
+for pos in range(1, len(sort_me2)):
+    number_of_times += 1
+    key_pos = pos
+    scan_pos = key_pos - 1
+    key_val = sort_me2[key_pos]
+
+    while scan_pos >= 0 and sort_me2[scan_pos] > key_val:
+        sort_me2[scan_pos + 1] = sort_me2[scan_pos]
+        scan_pos -= 1
+    sort_me2[scan_pos + 1] = key_val
+
+print("With insertion sorting: iterated through the list", number_of_times, "times.")
+
+
+
